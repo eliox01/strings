d68b73978b307949daf6c0f407ae41abf0645202
eliox01 <espejox01@gmail.com>
2020-03-06 03:04:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print('Hello there')
+name = input("What's is your name? ")
+
+print("It's nice to meet you", name)
+answer=input('Are you enjoying the course? ')
+if answer == (answer == 'Yes') or (answer == 'yes') :
+    print("that's good to hear !")
+else:
+    print("Oh no ! that's makes me sad! ")
